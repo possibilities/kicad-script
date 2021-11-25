2deefb5cccace392a9ec76472b04c5e2e84111d2
Mike Bannister <notimpossiblemike@gmail.com>
2021-11-24 20:51:10 -0500

    Getting/setting edge cut points

--- kicad_script/__init__.py
+++ kicad_script/__init__.py
@@ -1,3 +1,4 @@
+import uuid
 from pprint import pprint
 from sexpdata import loads, Symbol
 from types import SimpleNamespace
@@ -50,3 +51,32 @@ def get_nets(board):
 def add_net(board, name):
     next_id = len(get_nets(board))
     return (*board, (Symbol("net"), next_id, name))
+
+
+def set_edge_cut_points(board, points, width=0.1):
+    return (
+        *board,
+        *[
+            (
+                Symbol("gr_line"),
+                (Symbol("start"), point[0], point[1]),
+                (
+                    Symbol("end"),
+                    points[index + 1 if index < (len(points) - 1) else 0][0],
+                    points[index + 1 if index < (len(points) - 1) else 0][1],
+                ),
+                (Symbol("layer"), "Edge.Cuts"),
+                (Symbol("width"), width),
+                (Symbol("tstamp"), Symbol(uuid.uuid4())),
+            )
+            for index, point in enumerate(points)
+        ],
+    )
+
+
+def get_edge_cut_points(board):
+    return [
+        item[1][1:]
+        for item in board
+        if item[0] == Symbol("gr_line") and item[3][1] == "Edge.Cuts"
+    ]
--- test_create.py
+++ test_create.py
@@ -8,25 +8,20 @@ def test_create_board():
     assert board[0] == Symbol("kicad_pcb")
 
 
-def test_get_thickness():
-    board = k.create_board()
-    assert k.get_thickness(board) == 1.6
-
-
-def test_set_thickness():
+def test_set_and_get_thickness():
     board = k.create_board()
     board = k.set_thickness(board, 1.7)
     assert k.get_thickness(board) == 1.7
 
 
-def test_get_nets():
+def test_get_initial_nets():
     board = k.create_board()
     [initial_net] = k.get_nets(board)
     assert initial_net.id == 0
     assert initial_net.name == ""
 
 
-def test_add_nets():
+def test_add_and_get_nets():
     board = k.create_board()
     board = k.add_net(board, "Net 1")
     board = k.add_net(board, "Net 2")
@@ -35,3 +30,10 @@ def test_add_nets():
     assert added_net_1.name == "Net 1"
     assert added_net_2.id == 2
     assert added_net_2.name == "Net 2"
+
+
+def test_set_and_get_edge_cut_points():
+    board = k.create_board()
+    board = k.set_edge_cut_points(board, ((-5, -5), (5, -5), (5, 5), (-5, 5)))
+    print(str(dumps(board, pretty_print=True)))
+    assert k.get_edge_cut_points(board) == [(-5, -5), (5, -5), (5, 5), (-5, 5)]
